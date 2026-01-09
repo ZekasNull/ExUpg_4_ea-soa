c97733b923bdadc2b4543db903e91f8ec8ebc109
Zekas <zekasnull@pm.me>
2026-01-09 16:49:58 +0100

    Create GUI entry point class

---
+++ transfer_gui/src/main/java/se/ltu/transfer_gui/App.java
@@ -0,0 +1,27 @@
+package se.ltu.transfer_gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("CanvasTransfer.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Överför kalenderdata till Canvas");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args)
+    {
+        launch();
+    }
+}
