b46b3fad82734b648966689035f60506d2d82100
Zekas <zekasnull@pm.me>
2026-01-09 16:46:55 +0100

    Add Vendela's GUI fxml and controller skeleton to repo

---
+++ transfer_gui/src/main/java/se/ltu/transfer_gui/CanvasTransferController.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package se.ltu.transfer_gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodEvent;
+
+public class CanvasTransferController {
+
+    @FXML
+    private MenuItem BokningsPeriod;
+
+    @FXML
+    private DatePicker EndDatePicker;
+
+    @FXML
+    private MenuItem EnskildBokning;
+
+    @FXML
+    private DatePicker FromDatePicker;
+
+    @FXML
+    private SplitMenuButton Meny;
+
+    @FXML
+    private DatePicker RedigeraDatePicker;
+
+    @FXML
+    private TextField RedigeraLarareTextField;
+
+    @FXML
+    private TextField RedigeraLokalTextField;
+
+    @FXML
+    private TextField RedigeraLokalTextfield;
+
+    @FXML
+    private MenuButton RedigeraTidMeny;
+
+    @FXML
+    private Button SkickaTillCanvasButton;
+
+    @FXML
+    private TextField kursKodTextField;
+
+    @FXML
+    void UpdateraCanvas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getKursKod(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void hanteraBokningsPeriod(ActionEvent event) {
+
+    }
+
+    @FXML
+    void hanteraEnskildBokning(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void redigeraLarare(ActionEvent event) {
+
+    }
+
+    @FXML
+    void redigeraLokal(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void selectEndDate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectFromDate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectKursKod(ActionEvent event) {
+
+    }
+
+
+
+}
+
