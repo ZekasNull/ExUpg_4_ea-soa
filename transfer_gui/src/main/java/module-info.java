9328b1222ff00a5d4a7367910e30260b3dbda9b7
Zekas <zekasnull@pm.me>
2026-01-11 18:59:53 +0100

    Update module-info

--- transfer_gui/src/main/java/module-info.java
+++ transfer_gui/src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module se.ltu.transfer_gui {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jakarta.jakartaee.api;
+    requires tools.jackson.databind;
+    requires java.logging;
 
 
     opens se.ltu.transfer_gui to javafx.fxml;
